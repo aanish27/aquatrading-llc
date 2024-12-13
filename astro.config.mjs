58d513ebbb237a244ea49b85fc599f77eb34fa91
aanish27 <aanish2710@gmail.com>
2024-12-13 17:21:00 -0500

    Removed unnecessary Files and Commented Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://aanish27.astro.github.io',
-  base: '/agency-landing-page-Astrojs',
 });
 
